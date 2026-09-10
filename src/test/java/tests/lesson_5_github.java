a555acc0fa31df4acf2073d3a2f69ee799ba34e3
Anastasiia Belevich <anast.belevich@gmail.com>
2026-09-10 11:41:41 +0300

    добавила 2 вариант поиска локатора - тэг + текст

--- src/test/java/tests/lesson_5_github.java
+++ src/test/java/tests/lesson_5_github.java
@@ -11,6 +11,7 @@ public class lesson_5_github {
     void solutionsEnterprise() {
         open("https://github.com");
         $("nav").$(byText("Solutions")).hover();
+       // $(byTagAndText("button","Solutions")).hover();
         $(byText("Enterprises")).click();
         sleep(5000);
         $("[data-testid=Hero-grid]").$("span").shouldHave(text(("Bring your DevOps together on one secure platform")));
