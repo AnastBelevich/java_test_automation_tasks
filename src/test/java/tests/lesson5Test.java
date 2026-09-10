2e9cf18560baceed841de759970dd76d60a90d00
Anastasiia Belevich <anast.belevich@gmail.com>
2026-09-10 12:10:38 +0300

    убрала лишние строки

--- src/test/java/tests/lesson5Test.java
+++ src/test/java/tests/lesson5Test.java
@@ -27,26 +27,16 @@ class lesson5Test {
 
         // Откройте https://the-internet.herokuapp.com/drag_and_drop
         open("https://the-internet.herokuapp.com/drag_and_drop");
-
         // Перенесите прямоугольник А на место В
         //        Selenide.actions().clickAndHold($("#column-a"))
-         //               .moveToElement($("#column-b"))
-          //              .release()
-          //              .perform();
-
+        //               .moveToElement($("#column-b"))
+        //              .release()
+        //              .perform();
         $("#column-a").dragAndDrop(to($("#column-b")));
-
         // Проверьте, что прямоугольники действительно поменялись
         $("#column-a").shouldHave(text("B"));
         $("#column-b").shouldHave(text("A"));
-        // В Selenide есть команда $(element).dragAndDrop($(to-element)), проверьте работает ли тест, если использовать её вместо actions()
 
-        // $("nav").$(byText("Solutions")).hover();
-        // $(byTagAndText("button","Solutions")).hover();
-        // $(byText("Enterprises")).click();
-        // $("[href='/enterprise']").click();
-        // sleep(5000);
-        // $("[data-testid=Hero-grid]").$("span").shouldHave(text(("Bring your DevOps together on one secure platform")));
 
     }
 }
