4e1aaa72f9a7b0904ca845e2057f5a14554a54ff
Anastasiia Belevich <anast.belevich@gmail.com>
2026-09-10 12:08:53 +0300

    написан тест drag and drop

---
+++ src/test/java/tests/lesson5Test.java
@@ -0,0 +1,52 @@
+package tests;
+
+import com.codeborne.selenide.DragAndDropOptions;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.DragAndDropOptions.to;
+import static com.codeborne.selenide.Selenide.*;      // $, $$, open, sleep
+import static com.codeborne.selenide.Condition.*;     // text, visible, exist
+import static com.codeborne.selenide.Selectors.*;     // byText, byTagAndText
+
+class lesson5Test {
+    @Test
+    void solutionsEnterprise() {
+        open("https://github.com");
+        // $("nav").$(byText("Solutions")).hover();
+        $(byTagAndText("button","Solutions")).hover();
+        $(byText("Enterprises")).click();
+       // $("[href='/enterprise']").click();
+        sleep(5000);
+        $("[data-testid=Hero-grid]").$("span").shouldHave(text(("Bring your DevOps together on one secure platform")));
+
+    }
+
+    @Test
+    void dragAndDropActions() {
+
+        // Откройте https://the-internet.herokuapp.com/drag_and_drop
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+
+        // Перенесите прямоугольник А на место В
+        //        Selenide.actions().clickAndHold($("#column-a"))
+         //               .moveToElement($("#column-b"))
+          //              .release()
+          //              .perform();
+
+        $("#column-a").dragAndDrop(to($("#column-b")));
+
+        // Проверьте, что прямоугольники действительно поменялись
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+        // В Selenide есть команда $(element).dragAndDrop($(to-element)), проверьте работает ли тест, если использовать её вместо actions()
+
+        // $("nav").$(byText("Solutions")).hover();
+        // $(byTagAndText("button","Solutions")).hover();
+        // $(byText("Enterprises")).click();
+        // $("[href='/enterprise']").click();
+        // sleep(5000);
+        // $("[data-testid=Hero-grid]").$("span").shouldHave(text(("Bring your DevOps together on one secure platform")));
+
+    }
+}
